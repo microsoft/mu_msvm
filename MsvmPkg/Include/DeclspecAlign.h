528d98fbee32b16e478555bfff38cb3d5028ff59
Jay Krell <jaykrell@microsoft.com>
2026-02-27 18:15:25 +0000

    Merged PR 14895864: Clang: Reverse _MSC_VER checks.
    `_MSC_VER` may or may not be defined by Clang, depending on how compatible it is trying to be and how compatible it is.
    So check `__clang__` first.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This pull request is a bug fix aimed at improving compiler compatibility by reversing _MSC_VER checks for alignment attributes.
    
    #### PR Summary
    The change updates the macro in `/MsvmPkg/Include/DeclspecAlign.h` to ensure that when compiling with Clang or GNUC (or when _MSC_VER is not defined), the `__attribute__((aligned(n)))` is used instead of the MSVC-specific `__declspec(align(n))`.
    - `/MsvmPkg/Include/DeclspecAlign.h`: Reversed the condition to correctly select the alignment attribute based on the compiler.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #54710354, #57987809

--- MsvmPkg/Include/DeclspecAlign.h
+++ MsvmPkg/Include/DeclspecAlign.h
@@ -7,9 +7,9 @@
 #ifdef __cplusplus
 #define DECLSPEC_ALIGN(n) alignas (n)
 #else
-#ifdef _MSC_VER
-#define DECLSPEC_ALIGN(n) __declspec (align (n))
-#else
+#if defined (__clang__) || defined (__GNUC__) || !defined (_MSC_VER)
 #define DECLSPEC_ALIGN(n) __attribute__ ((aligned (n)))
+#else
+#define DECLSPEC_ALIGN(n) __declspec (align (n))
 #endif
 #endif
