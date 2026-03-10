13ec431dae11932ab2d0c857aaff52e0d4d379ad
Jay Krell <jaykrell@microsoft.com>
2026-03-10 06:53:18 +0000

    Merged PR 14965745: Gcc: Cast ignored expression to void (STATIC_ASSERT_EXPRESSION).
    ```
    INFO - /home/jay/hyperv.uefi/MsvmPkg/Include/StaticAssertExpression.h:8:37: error: left-hand operand of comma expression has no effect [-Werror=unused-value]
    INFO -     8 |     (sizeof (char [(expr) ? 1 : -1]), (value))
    INFO -       |                                     ^
    INFO - /home/jay/hyperv.uefi/MsvmPkg/Include/AssignStruct.h:17:9: note: in expansion of macro STATIC_ASSERT_EXPRESSION
    ```
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    This PR performs a code cleanup by adding an explicit cast to void in a static assertion macro.
    
    #### PR Summary
    The pull request updates the `STATIC_ASSERT_EXPRESSION` macro in `MsvmPkg/Include/StaticAssertExpression.h` to explicitly cast the `sizeof` expression to void, eliminating compiler warnings and supporting the AARCH64 Clang build as outlined in the linked work items.
    - `MsvmPkg/Include/StaticAssertExpression.h`: Modified the macro to cast the `sizeof` expression to void.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #57987809

--- MsvmPkg/Include/StaticAssertExpression.h
+++ MsvmPkg/Include/StaticAssertExpression.h
@@ -5,4 +5,4 @@
 // Compile iff 'expr' is true, yielding 'value'.
 // see also: Common\MU_TIANO\SecurityPkg\DeviceSecurity\SpdmLib\libspdm\os_stub\mbedtlslib\mbedtls\library\common.h
 #define STATIC_ASSERT_EXPRESSION(expr, value) \
-    (sizeof (char [(expr) ? 1 : -1]), (value))
+    (((void)sizeof (char [(expr) ? 1 : -1])), (value))
