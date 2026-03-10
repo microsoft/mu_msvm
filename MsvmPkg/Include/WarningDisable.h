b9b6c11f9401d35c29b3471cb84b2cbb30e90948
Jay Krell <jaykrell@microsoft.com>
2026-03-10 18:51:24 +0000

    Merged PR 14980905: Gcc: Fix more pragma warning and update PACKET_LIB_CONTEXT.
    Every pragma warning is a warning or error with gcc.
    I fixed most of the, but somehow missed some.
    
    ----
    #### AI description  (iteration 2)
    #### PR Classification
    This PR fixes pragma warning handling across compilers and updates PACKET_LIB_CONTEXT to improve cross-compiler compatibility.
    
    #### PR Summary
    The pull request standardizes warning suppression by introducing new macros for Clang, GCC, and MSVC, while also updating the PACKET_LIB_CONTEXT structure with reserved fields.
    - `/MsvmPkg/Include/Pragma.h`: Added and conditionally defined `CLANG_PRAGMA`, `GCC_PRAGMA`, and `MS_PRAGMA` macros.
    - `/MsvmPkg/Include/WarningDisable.h`: Updated warning disable macros to use the new pragma macros.
    - `/MsvmPkg/Include/Vmbus/VmbusPacketInterface.h`: Replaced direct pragma directives with standardized macros and added `Reserved[2]` to `PACKET_LIB_CONTEXT`.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #57987809

--- MsvmPkg/Include/WarningDisable.h
+++ MsvmPkg/Include/WarningDisable.h
@@ -2,18 +2,6 @@
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 #pragma once
 #include "Pragma.h"
-
-#define CLANG_WARNING_DISABLE(x)
-#define GCC_WARNING_DISABLE(x)
-#define MS_WARNING_DISABLE(x)
-
-#if defined (__clang__)
-#undef CLANG_WARNING_DISABLE
-#define CLANG_WARNING_DISABLE(x) PRAGMA(clang diagnostic ignored #x)
-#elif defined (__GNUC__)
-#undef GCC_WARNING_DISABLE
-#define GCC_WARNING_DISABLE(x) PRAGMA(GCC diagnostic ignored #x)
-#elif defined (_MSC_VER)
-#undef MS_WARNING_DISABLE
-#define MS_WARNING_DISABLE(x) PRAGMA(warning(disable:x))
-#endif
+#define CLANG_WARNING_DISABLE(x) CLANG_PRAGMA(clang diagnostic ignored #x)
+#define GCC_WARNING_DISABLE(x) GCC_PRAGMA(GCC diagnostic ignored #x)
+#define MS_WARNING_DISABLE(x) MS_PRAGMA(warning(disable:x))
