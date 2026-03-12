99ab9f99f6a578129bf60c8e343750c434ae4619
Jay Krell <jaykrell@microsoft.com>
2026-03-12 20:53:18 +0000

    Merged PR 15004216: Gcc/clang: Remove implicit memcpy/memset (AziHsmBKS3.c).
    `char big[99]= "small"`
    is `memcpy` and/or `memset`.
    
    In this case, the copy is not needed. (In other cases we often add CopyMem/ZeroMem calls as part of the fix).
    
    Related work items: #57987809

---
+++ MsvmPkg/Include/MinMaxType.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+#pragma once
+#define MIN_UINT8  0u
+#define MIN_UINT16 0u
+#define MIN_UINT32 0u
+#define MIN_UINT64 0u
+#define MIN_TYPE(type) MIN_##type
+#define MAX_TYPE(type) MAX_##type
