c427e54198af7cfdc1be3129a1728ddfb494f731
Jay Krell <jaykrell@microsoft.com>
2026-03-12 20:53:18 +0000

    Merged PR 15004216: Gcc/clang: Remove implicit memcpy/memset (AziHsmBKS3.c).
    `char big[99]= "small"`
    is `memcpy` and/or `memset`.
    
    In this case, the copy is not needed. (In other cases we often add CopyMem/ZeroMem calls as part of the fix).
    
    Related work items: #57987809

---
+++ MsvmPkg/Include/CastConstant.h
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+#pragma once
+#include "MinMaxType.h"
+#include "StaticAssertExpression.h"
+// If value is unsigned and type is signed, this always fails.
+#define CAST_CONSTANT(value, type) \
+  STATIC_ASSERT_EXPRESSION(value >= MIN_TYPE(type) && value <= MAX_TYPE(type), (type)(value))
