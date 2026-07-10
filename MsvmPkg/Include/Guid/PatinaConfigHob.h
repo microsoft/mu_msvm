7ee4141f407d0bb7b6e16bec29761ddd700520ed
Oliver Smith-Denny <osde@microsoft.com>
2026-07-10 15:36:29 -0700

    Add Patina Config HOB
    MSVM AARCH64 can have dynamic GIC bases, which
    is a configuration input to Patina. This produces
    a HOB with the actual values so that Patina can
    configure them correctly.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

---
+++ MsvmPkg/Include/Guid/PatinaConfigHob.h
@@ -0,0 +1,37 @@
+/** @file -- PatinaConfigHob.h
+
+Contains definitions for the HOB to pass config to Patina.
+This must be kept in sync with msvm-patina-bins.
+
+Copyright (c) Microsoft Corporation.
+
+**/
+
+#ifndef PATINA_CONFIG_HOB_H_
+#define PATINA_CONFIG_HOB_H_
+
+#pragma pack(push, 1)
+
+//
+// This HOB is produced to pass configuration to the MSVM Patina bin wrapper.
+//
+typedef struct _MSVM_PATINA_CONFIG {
+  UINT32                  VersionMajor;
+  UINT32                  VersionMinor;
+ #if defined (MDE_CPU_AARCH64)
+  EFI_PHYSICAL_ADDRESS    GicDistributorBase;
+  EFI_PHYSICAL_ADDRESS    GicRedistributorBase;
+ #endif
+} MSVM_PATINA_CONFIG;
+
+#pragma pack(pop)
+
+#define MSVM_CONFIG_HOB_GUID \
+  { 0x1feb5a86, 0x7695, 0x4a5c, { 0x93, 0x49, 0x7d, 0x24, 0x3a, 0x43, 0x99, 0x49 } }
+
+extern EFI_GUID  gMsvmPatinaConfigHob;
+
+#define MSVM_CONFIG_HOB_VERSION_MAJOR  1
+#define MSVM_CONFIG_HOB_VERSION_MINOR  0
+
+#endif // PATINA_CONFIG_HOB_H_
